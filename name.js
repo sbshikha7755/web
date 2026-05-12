fa71da860dd75ba39f21514af8d104bcdd6d8068
sbshikha7755 <sbshikha7755@gmail.com>
2026-05-12 13:01:36 +0530

    Kashi Tour

--- name.js
+++ name.js
@@ -1,1 +1,39 @@
-package
+console.log("i am shikha");
+a=20
+b=10
+c=a+b
+console.log(c);
+ 
+console.log("var");
+var a = 10
+// console.log(a);
+
+a=60
+// console.log(a);
+
+{
+    var a = 40;
+}
+console.log(a);
+
+let ab = 90;
+// console.log(ab);
+
+ab =45;
+ab=78;
+{
+    let ab=100;
+    console.log(ab)
+}
+console.log(ab);
+let x;
+console.log(x); 
+
+// console.log("const");
+
+// const d=5;
+// console.log(d);
+//  d=10;
+// console.log(7);
+
+
